d537f927e065a0e7b4780997276486ed051bf078
djamara <djamaracyrille@gmail.com>
2021-03-04 15:35:56 +0000

    04/03/2021

--- src/app/activite.service.ts
+++ src/app/activite.service.ts
@@ -9,8 +9,18 @@ export class ActiviteService {
   constructor(private http: HttpClient) { }
 
   private activiteUrl = 'http://localhost:8000/api/activite';
+  private activiteByMonthUrl = 'http://localhost:8000/api/ActiviteByMonth';
+  private activiteByMonthByProjetUrl = 'http://localhost:8000/api/ActiviteByMonthByProjetID?id=';
   // tslint:disable-next-line:typedef
   getActivite() {
     return this.http.get(this.activiteUrl);
+  }
+  // tslint:disable-next-line:typedef
+  getActiviteByMonth() {
+    return this.http.get(this.activiteByMonthUrl);
+  }
+  // tslint:disable-next-line:typedef
+  getActiviteByMonthByProjet(id){
+    return this.http.get(this.activiteByMonthByProjetUrl + id);
   }
 }
